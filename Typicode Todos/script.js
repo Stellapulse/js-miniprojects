f37f4896c4541a314f4ebe3620e6a47b4ebed51a
Stellapulse <akarshtripathi111111@gmail.com>
2025-12-10 12:50:41 +0530

    Clean up code formatting and improve readability

--- Typicode Todos/script.js
+++ Typicode Todos/script.js
@@ -4,18 +4,45 @@ const gettodos=()=>{
     fetch(apiUrl + '?_limit=5')//Fetches 5 todos from the API
     .then((res)=> res.json())
     .then((data) =>{
-        data.forEach((todo) => {
-            const div = document.createElement('div');
+        data.forEach((todo) => addTodoToDom(todo));
+    console.log(data)
+});
+};
+
+const addTodoToDom = (todo) =>
+{
+      const div = document.createElement('div');
             div.appendChild(document.createTextNode(todo.title));
-             
+            div.setAttribute('data-id',todo.id);
             if(todo.completed){
                 div.classList.add('done');
             }
 
             document.getElementById('todo-list').appendChild(div);
-           })
-    console.log(data)
-});
-};
+}
 
-gettodos();
+const createTodo = (e) => {
+  e.preventDefault();//Prevents the default form submission behavior
+//   console.log(e.target.firstElementChild.value);
+
+  const newTodo = {
+     title:e.target.firstElementChild.value,
+     completed:false
+  }
+    fetch(apiUrl,{
+        method:'POST',
+        body:JSON.stringify(newTodo),
+        headers:{   
+            'Content-type':'application/json'
+        },
+    })
+    .then((res) => res.json())
+    .then((data) => addTodoToDom(data)); 
+}
+
+const init =() =>{
+
+    document.addEventListener('DOMContentLoaded', gettodos);
+    document.querySelector('#todo-form').addEventListener('submit',createTodo);
+}
+init();
