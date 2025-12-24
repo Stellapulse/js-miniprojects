7a8fa5bd21d1e5f61917cfdb710433dbb44cd991
Stellapulse <akarshtripathi111111@gmail.com>
2025-12-24 11:30:41 +0530

    first mini Animation project

---
+++ RequestAnimationFrame/script.js
@@ -0,0 +1,28 @@
+ const soccer= document.querySelector('img');
+let start;
+let done=false;
+
+function step(timestamp){
+
+      console.log(timestamp);
+
+    if(start===undefined){
+        start=timestamp;
+    }
+    const elapsed=timestamp - start;
+
+   
+    soccer.style.transform=`translateX(${elapsed/20}px) rotate(${elapsed/10}deg)`;
+
+    if(elapsed>5000){
+        done=true;
+
+    }
+
+    if(done){
+        return;
+    }
+
+    requestAnimationFrame(step);
+}
+requestAnimationFrame(step);
